c1bc04119fbf6428e6cd409cd7a93ac1732c7aee
Kaylane Coutinho <149087078+kaycout@users.noreply.github.com>
2025-04-15 10:25:32 -0300

    Novas modificações da conexão do backend
    Novas modificações nos arquivos de conexão no backend

--- js/script.js
+++ js/script.js
@@ -1,65 +1,3 @@
-function atualizada(id,nome,email,usuario){
-
-    if(confirm("Você deseja atualizar este cliente?")==1){
-       document.getElementById("form").style.display="block";
-        const form_nome = document.getElementById("txtnome");
-        const form_email = document.getElementById("txtemail");
-        const form_usuario = document.getElementById("txtusuario");
-        const form_senha = document.getElementById("txtsenha");
-
-        form_nome.value = nome;
-        form_email.value = email;
-        form_usuario.value = usuario;
-
-        //ocultar o campo senha
-        document.getElementById("txtsenha").style.display="none";
-
-        //ocultar o botão cadastrar 
-        document.getElementsByTagName("input")[4].style.display="none";
-
-        //criar um botao para o formulario de atualizacao
-        const btn_atualizar = document.createElement("button");
-
-        //adicionar um texto ao botao
-        btn_atualizar.innerHTML = "Atualizar"
-
-        //adicionar um id ao nosso botao
-        btn_atualizar.setAttribute("id","btn-atualizar");
-        //adicionar um atributo de chamada de funcao
-        btn_atualizar.setAttribute("onclick",`executar_atualizacao(${id})`)
-        //adicionar o botao atualizar no formulario
-
-        document.getElementById("form").appendChild(btn_atualizar);
-  
-    }
-}
-
-function executar_atualizacao(id){
-    alert(id);
-}
-
-function excluir(id){
-
-    if(confirm("Você deseja apagar este cliente?")==1){
-        fetch(`http://127.0.0.1:3000/apagar/${id}`,{
-            method:"DELETE",
-            headers:{
-                "accept":"application/json",
-                "content-type":"application/json"
-            }
-        })
-
-        .then((response)=>response.json())
-        .then((dados)=>{
-            alert(`Cliente apagado!`);
-            //recarega a página, ou seja, atualiza a página.
-           document.location.reload();
-        })
-
-        .catch((erro)=>console.error(erro))
-    }
-}
-
 function carregar(){
     //vamos fazer uma referencia ao container que está 
     //na página e, assim manipularmos a div container 
@@ -78,9 +16,7 @@ function carregar(){
             	saida+=`<div class="cliente">
                 <p>Id:${cli.id}</p>
                 <h2>Nome:${cli.nome}</h2>
-                <h3>E-mail:${cli.email}</h3>
-                <img src="img/atualizada.png" id="atualizada" onclick="atualizada(${cli.id},'${cli.nome}','${cli.email}','${cli.usuario}')">
-                <img src="img/excluir.png" id="excluir" onclick="excluir(${cli.id})">
+                <h2>E-mail:${cli.email}</h3>
                 </div>`
         })
         container.innerHTML = saida;
@@ -127,9 +63,4 @@ fetch("http://127.0.0.1:3000/cadastrar",{
 const cad = document.getElementById("cadastro");
 cad.onclick = ()=>{
     document.getElementById("form").style.display = "block";
-}
-
-const btnlogin = document.getElementById("login");
-btnlogin.onclick = ()=>{
-    document.getElementsByClassName("sombra")[0].style.top="0px";
 }
